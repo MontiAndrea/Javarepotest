ec1d094a9a93e44695928d2c5f76fbbd378b1dac
monti.andrea <monti.andrea@TAVE-PC08.OSDBSESTO.local>
2020-11-10 13:02:58 +0100

    add endless loop

---
+++ src/provagithub/Provagithub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagithub;
+
+/**
+ *
+ * @author monti.andrea
+ */
+public class Provagithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 2;
+        while(true){
+           System.out.println("ehi ciao"+i);
+           i=i*i;
+        }
+    }
+    
+}
